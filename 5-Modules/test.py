4f661d29cc850d18f2c43772004f9fba56b82f42
Sundar R K <sundarrk85@gmail.com>
2025-03-23 00:22:26 +0530

    My first commit after setting up Python environment

---
+++ 5-Modules/test.py
@@ -0,0 +1,9 @@
+from package.maths import *
+from package.subpackage import mult
+
+
+print(f'The multiplication result of 5 and 6 is {mult.multiply(5,6)}')
+
+print(addition(4,5))
+print(substraction(5,6))
+
